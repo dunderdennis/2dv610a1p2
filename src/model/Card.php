471a0912ec52484b344cf2c56345e0526f5a67c6
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-11-26 20:42:43 +0100

    GREEN

--- src/model/Card.php
+++ src/model/Card.php
@@ -5,14 +5,30 @@ namespace model;
 class Card
 {
     private $gameValue;
+    private $color;
 
     public function __construct(int $cardIndex)
     {
+        if ($cardIndex <= 13) {
+            $this->color = "hearts";
+        } else if ($cardIndex <= 26) {
+            $this->color = "diamonds";
+        } else if ($cardIndex <= 39) {
+            $this->color = "clubs";
+        } else if ($cardIndex <= 52) {
+            $this->color = "spades";
+        }
+
         $this->gameValue = ($cardIndex % 10) + 1;
     }
 
     public function getGameValue()
     {
         return $this->gameValue;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
     }
 }
