63c27f797fff36a6804106ab9115a4ab35db863f
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-11-26 20:35:35 +0100

    REFACTOR

--- test/model/CardTest.php
+++ test/model/CardTest.php
@@ -2,12 +2,25 @@
 
 class CardTest extends PHPUnit\Framework\TestCase
 {
-    /** @test */
-    public function shouldHaveValidGameValue()
+    /** @test 
+     *  @dataProvider validCardIndexesProvider
+     */
+    public function shouldHaveValidGameValue($cardIndex)
     {
-        $sut = new \model\Card(8);
+        $sut = new \model\Card($cardIndex);
         $actual = $sut->getGameValue();
 
         $this->assertTrue(1 <= $actual && $actual <= 10, 'Game value of a card must be 1-10.');
+    }
+
+    public function validCardIndexesProvider()
+    {
+        $cardIndexes = array();
+        for ($i = 1; $i <= 52; $i++) {
+            $cardIndex = array();
+            array_push($cardIndex, $i);
+            array_push($cardIndexes, $cardIndex);
+        }
+        return $cardIndexes;
     }
 }
