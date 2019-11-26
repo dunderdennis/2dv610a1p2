06f7f3ddf915f25612eda133de676a1e06c45593
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-11-26 20:40:36 +0100

    RED

--- test/model/CardTest.php
+++ test/model/CardTest.php
@@ -13,10 +13,33 @@ class CardTest extends PHPUnit\Framework\TestCase
         $this->assertTrue(1 <= $actual && $actual <= 10, 'Game value of a card must be 1-10.');
     }
 
+    /** @test 
+     *  @dataProvider CardIndexesWithHeartsColorProvider
+     */
+    public function shouldHaveColorHearts($cardIndex)
+    {
+        $sut = new \model\Card($cardIndex);
+        $actual = $sut->getColor();
+        $expected = 'hearts';
+
+        $this->assertEquals($actual, $expected, 'Card indexes 1-13 should have color: hearts.');
+    }
+
     public function validCardIndexesProvider()
     {
         $cardIndexes = array();
         for ($i = 1; $i <= 52; $i++) {
+            $cardIndex = array();
+            array_push($cardIndex, $i);
+            array_push($cardIndexes, $cardIndex);
+        }
+        return $cardIndexes;
+    }
+
+    public function CardIndexesWithHeartsColorProvider()
+    {
+        $cardIndexes = array();
+        for ($i = 1; $i <= 13; $i++) {
             $cardIndex = array();
             array_push($cardIndex, $i);
             array_push($cardIndexes, $cardIndex);
