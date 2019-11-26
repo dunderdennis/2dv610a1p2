36b53465710e39e4d2c6b6fc5807d24f039052a9
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-11-26 20:47:53 +0100

    REFACTOR

--- src/index.php
+++ src/index.php
@@ -4,7 +4,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 ini_set('display_startup_errors', 'On');
 
+
+require_once "model/Card.php";
+
 require_once "view/GameView.php";
+
 
 $gameView = new GameView();
 
--- src/view/GameView.php
+++ src/view/GameView.php
@@ -4,9 +4,6 @@ namespace view;
 
 class GameView
 {
-    public function __construct()
-    { }
-
     public function generateWelcomeMessage()
     {
         return "Welcome to this blackjack game!";
